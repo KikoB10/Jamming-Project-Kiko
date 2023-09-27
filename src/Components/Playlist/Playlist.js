16972dcb44ffdc5e636126869d376a84ed1fa571
kikob <burten.kiko@gmail.com>
2023-09-27 17:19:05 -0500

    css cleaner, still needs final updating to actually make it look nice

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,7 +6,11 @@ function Playlist() {
   return (
     <div className="Playlist">
       <h2>Songs added</h2>
+
       <Tracklist />
+      <div className="Playlist-name">
+        <input placeholder="Enter unique playlist name" />
+      </div>
       <button>Save to Spotify</button>
     </div>
   );
